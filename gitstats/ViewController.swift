c60569a84360ee789c3d47db9067677cdecc3329
michal.kempski@sap.com <michal.kempski@sap.com>
2019-05-24 15:36:15 +0200

    Initial Commit

---
+++ gitstats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitstats
+//
+//  Created by Kempski, Michal on 24/05/2019.
+//  Copyright © 2019 Kempski, Michal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
